673b2e71cb085eae97efa2a4f9803a74375e7628
Kirill Blinov <60810096+skips0skips@users.noreply.github.com>
2023-03-23 16:43:07 +0300

    Update Storage of datasets.sol
    Добавлены функции для работы с балансом

--- Storage of datasets.sol
+++ Storage of datasets.sol
@@ -56,4 +56,30 @@ contract DataLoader {
         Data memory externalData = dataContract.getData(count - 1);
         addData(externalData.name, externalData.values);
     }
+
+    // Функция для получения данных по ID.
+    function getData(uint _id) public view returns (Data memory) {
+        require(_id < data.length, "Invalid data id");
+        return data[_id];
+    }
+
+    // Функция для пополнения баланса пользователя.
+    function deposit() public payable {
+        uint amount = msg.value / (1 ether);
+        balances[msg.sender] += amount;
+        emit Deposit(msg.sender, amount);
+    }
+
+    // Функция для вывода средств с баланса пользователя на кошелек.
+    function withdraw(uint amount) public {
+        require(balances[msg.sender] >= amount, "Insufficient balance");
+        balances[msg.sender] -= amount;
+        payable(msg.sender).transfer(amount * (1 ether));
+        emit Withdrawal(msg.sender, amount);
+    }
+
+    // Функция проверки баланса.
+    function balanceOf() public view returns (uint) {
+        return address(this).balance / (1 ether);
+    }
 }
